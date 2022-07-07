d609119bc2008ef405139bcca37cbb68ff42d1dc
levi.borodenko <levi.borodenko@smart-steel-technologies.com>
2022-07-07 15:23:51 +0200

    typo in lsp name

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ M.setup_lsp = function(attach, capabilities)
   local lspconfig = require "lspconfig"
 
   -- lspservers with default config
-  local servers = { "html", "pyright", "yamlls", "dockerls", "bashls", "typescript", "vuels" }
+  local servers = { "html", "pyright", "yamlls", "dockerls", "bashls", "tsserver", "vuels" }
 
   for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
