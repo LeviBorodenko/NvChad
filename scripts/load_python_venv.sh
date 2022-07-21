87512c8520dce311ad7eecb945e0f29a1b01419f
levi.borodenko <levi.borodenko@smart-steel-technologies.com>
2022-07-21 14:55:31 +0200

    only create venv in top level

--- scripts/load_python_venv.sh
+++ scripts/load_python_venv.sh
@@ -6,9 +6,14 @@ elif [[ -d "venv" ]]; then
 elif [[ -d ".venv" ]]; then
   source .venv/bin/activate
 else
-  echo 'Creating venv'
-  python3.10 -m venv venv
+   
+   if [[ -d ".git" ]]; then
+     echo 'Creating venv'
+     python3.10 -m venv venv
 
-  # adding venv to .gitignore if not present
-  grep -qxF 'venv' .gitignore || echo 'venv' >> .gitignore
+     # adding venv to .gitignore if not present
+     grep -qxF 'venv' .gitignore || echo 'venv' >> .gitignore
+   else
+      echo 'No venv found and none will be created.'
+   fi
 fi
